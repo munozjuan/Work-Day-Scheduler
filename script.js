f47bbe5848047f73e2e7e59dab02d8dbb1b5c686
munozjuan <munoz.juan07@hotmail.com>
2020-11-14 00:43:47 -0500

    added current time var

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
-    // Time Array of the hours in our scheduler"
+    // Time Array of the hours in our scheduler
     var times = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+    var currentTime = moment().format("x y");
 })
 
