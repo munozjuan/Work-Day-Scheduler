d89726bdfd246603a702f9d284a9de856e677b60
munozjuan <munoz.juan07@hotmail.com>
2020-11-14 00:35:24 -0500

    added time array

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
 $(document).ready(function(){
-    /* Display the current day of the week and date
-    at the top of the page */
+    /* Display the current date at the top */
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    
-    var timeArray = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
-    var currentTime = moment().format("h A");
-    
-    
-    // Set each span text to the correct hour between 9am and 5pm
-    $(".Hour").each(function(i){
-        $(this).text(moment().hour(i+9).format("h A"))
-        
-    })
-    console.log(currentTime);
-    
-    
-    
-    $('.HourRow').each(function(i) {
-        console.log(timeArray.indexOf(currentTime));
-        if (timeArray.indexOf(currentTime) > i) {
-          $(`#${i}`).addClass('past');
-          $(`#saveToDo-${i}`).prop('disabled', true);
-          $(`#saveToDo-${i}`).addClass('past');
-          $(`#hour-${i}`).addClass('past');
-        } else if (timeArray.indexOf(currentTime) == i) {
-          $(`#${i}`).addClass('present');
-          $(`#saveToDo-${i}`).addClass('present');
-          $(`#saveToDo-${i}`).prop('disabled', false);
-          $(`#hour-${i}`).addClass('present');
-        } else if (timeArray.indexOf(currentTime) < i) {
-          $(`#${i}`).addClass('future');
-          $(`#saveToDo-${i}`).addClass('future');
-          $(`#saveToDo-${i}`).prop('disabled', false);
-          $(`#hour-${i}`).addClass('future');
-        }
-      });
-    
-    });
+
+
+    // Time Array of the hours in our scheduler"
+    var times = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+})
+
